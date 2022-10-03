dd6b78253dab2e2dd96d323523ad53b5e29d597e
astriafebiola <astriaf@mhs.upr.ac.id>
2022-10-03 10:56:13 +0700

    add screens configuration

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+// File to enable tailwindcss intellisense
+module.exports = {
+    theme: {
+    extend: {
+        screens: {
+        'sm': '320px',
+        // => @media (min-width: 320px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+      },
+    }
+  }
+}
